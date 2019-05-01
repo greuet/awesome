8d3d9e45899acab1cc23576fb97cda801d13023a
greuet <aureliengreuet@gmail.com>
2019-05-01 10:57:38 +0200

    bg arrows for launcher icons

--- themes/mypowerarrow/theme.lua
+++ themes/mypowerarrow/theme.lua
@@ -462,31 +462,43 @@ function theme.at_screen_connect(s)
                                        { s.mytaglist,
                                          layout = wibox.layout.align.horizontal },
                                        0, 3), theme.taglist_bg_normal .. "50"),
-             arrow_right(theme.taglist_bg_normal .. "50", theme.mybuttons_bg .. "90"),
+             arrow_right(theme.taglist_bg_normal .. "50", "#222222" .. "F0"),
              wibox.container.background(
                 wibox.container.margin(wibox.widget
                                        { terminal_button,
-                                         firefox_button,
-                                         emacs_button,
-                                         gimp_button,
-                                         darktable_button,
-                                         audacious_button,
                                          layout = wibox.layout.align.horizontal },
-                                       5, 3), theme.mybuttons_bg .. "90"),
+                                       5, 3), "#222222" .. "F0"),
+             arrow_right("#222222" .. "F0", "#343434" .. "F0"),
              wibox.container.background(
                 wibox.container.margin(wibox.widget
-                                       { thunar_button,
-                                         gimp_button,
-                                         darktable_button,
-                                         audacious_button,
+                                       { firefox_button,
                                          layout = wibox.layout.align.horizontal },
-                                       0, 3), theme.mybuttons_bg .. "90"),
+                                       5, 3), "#343434" .. "F0"),
+             arrow_right("#343434" .. "F0", "#777E76" .. "B0"),
+             wibox.container.background(
+                wibox.container.margin(wibox.widget
+                                       { emacs_button,
+                                         layout = wibox.layout.align.horizontal },
+                                       5, 3), "#777E76" .. "B0"),
+             arrow_right("#777E76" .. "B0", "#222222" .. "F0"),
+             wibox.container.background(
+                wibox.container.margin(wibox.widget
+                                       { gimp_button,
+                                         layout = wibox.layout.align.horizontal },
+                                       5, 3), "#222222" .. "F0"),
+             arrow_right("#222222" .. "F0", "#343434" .. "F0"),
+             wibox.container.background(
+                wibox.container.margin(wibox.widget
+                                       { darktable_button,
+                                         layout = wibox.layout.align.horizontal },
+                                       5, 3), "#343434" .. "F0"),
+             arrow_right("#343434" .. "F0", "#777E76" .. "B0"),
              wibox.container.background(
                 wibox.container.margin(wibox.widget
                                        { audacious_button,
                                          layout = wibox.layout.align.horizontal },
-                                       0, 3), theme.mybuttons_bg .. "90"),
-             arrow_right(theme.mybuttons_bg .. "90", "alpha"),
+                                       5, 3), "#777E76" .. "B0"),
+             arrow_right("#777E76" .. "B0", "alpha"),
              s.mypromptbox,
              spr,
           },
